29fefb6ebd13e0240697a57756d5556a99578b3f
D-Bednarczyk <117660494+D-Bednarczyk@users.noreply.github.com>
2023-06-21 18:10:46 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ const axios = require('axios').default;
 const API_URL = 'https://pixabay.com/api/';
 const API_KEY = '36133466-dbc0c7a3178523b048b6e9d9a';
 
-export const fetchImgs = async (searchQuery, page) => {
+const fetchImgs = async (searchQuery, page) => {
   try {
     const response = await axios.get(
       `${API_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`
