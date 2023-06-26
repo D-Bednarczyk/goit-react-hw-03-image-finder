ae2b3904df38d770df612bfaf87c0407436ce2a6
D-Bednarczyk <117660494+D-Bednarczyk@users.noreply.github.com>
2023-06-26 14:14:53 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import css from './App.module.css';
 
 import { fetchImgs } from '../Service/fetchImgs';
-import { Searchbar } from '../Searchbar/Searchbar';
+import { Searchbar } from './Searchbar/Searchbar';
 import { Loader } from './Loader/Loader';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
